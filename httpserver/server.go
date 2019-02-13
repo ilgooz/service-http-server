761defbdc08327e99a78aadfb8f53a9fa267199f
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-02-13 12:31:48 +0300

    support CORS

--- httpserver/server.go
+++ httpserver/server.go
@@ -42,6 +42,13 @@ type requestEventData struct {
 }
 
 func (s *HTTPServerService) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	if req.Method == "OPTIONS" {
+		return
+	}
+
 	se, err := newSession(w, req)
 	if err != nil {
 		log.Println(err)
