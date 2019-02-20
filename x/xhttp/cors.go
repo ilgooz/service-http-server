5c69cce831addbb1f275358a9f6aa3b5df9c9a89
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-02-20 13:54:27 +0300

    cors: support graphql headers & simplify url query to json encoding

--- x/xhttp/cors.go
+++ x/xhttp/cors.go
@@ -6,5 +6,5 @@ import "net/http"
 func CORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-apollo-tracing")
 }
--- x/xhttp/query.go
+++ x/xhttp/query.go
@@ -1,16 +1,11 @@
 package xhttp
 
 import (
+	"encoding/json"
 	"net/http"
-
-	"github.com/joncalhoun/qson"
 )
 
 // JSONQuery converts query string to json encoded byte slice.
 func JSONQuery(req *http.Request) ([]byte, error) {
-	qs := req.URL.Query().Encode()
-	if qs != "" {
-		return qson.ToJSON(qs)
-	}
-	return []byte("{}"), nil
+	return json.Marshal(req.URL.Query())
 }
